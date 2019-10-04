304b4c0238154124a29a513cc93ec794e3b7728d
Mauricio Flor <duvamalicio3d@hotmail.com>
2019-10-03 20:51:03 -0500

    Updates

---
+++ precache-manifest.70edb619d04c8ad119087130ec720087.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0319b4ef41e1d9a2f527163cfc38419",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "135ec95bd2cdb9419e51",
+    "url": "/monsters-rolodex/static/css/main.18d77aa8.chunk.css"
+  },
+  {
+    "revision": "0168801a1a0414135253",
+    "url": "/monsters-rolodex/static/js/2.4e5d8231.chunk.js"
+  },
+  {
+    "revision": "135ec95bd2cdb9419e51",
+    "url": "/monsters-rolodex/static/js/main.53094690.chunk.js"
+  },
+  {
+    "revision": "c4a009f5d3a5fc9c4346",
+    "url": "/monsters-rolodex/static/js/runtime-main.205b164d.js"
+  }
+]);
